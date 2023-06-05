291da16b2085087866493abcfdf2ec62d47713bb
Louisa Ongenyi <o.louisa@ymail.com>
2023-06-05 22:04:42 +0200

    Page edit Name fonctionnelle

--- Front-end/mon-app/src/components/SignOut.js
+++ Front-end/mon-app/src/components/SignOut.js
@@ -2,39 +2,55 @@ import React from 'react';
 import "../styles/pages.css";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCircleUser} from '@fortawesome/free-solid-svg-icons';
-import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { clearDatas } from '../actions';
+import { useLocation } from 'react-router-dom';
+import ProfileEdit from "../assets/img/profileEdit.png";
+import Settings from "../assets/img/settings.png";
+import Deconnect from "../assets/img/deconnect.png";
 
-function SignOut({style}) {
- let username = useSelector((state) => state.username);
- const dispatch = useDispatch();
- let token = useSelector((state) => state.token);
- let id = useSelector((state) => state.id);
+function SignOut({ style }) {
+  let username = useSelector((state) => state.username);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  let token = useSelector((state) => state.token);
+  let id = useSelector((state) => state.id);
 
-const handleClick = () => {
-  dispatch(clearDatas());
-  localStorage.clear();
-  console.log(username, token, id);
+  const handleClick = () => {
+    dispatch(clearDatas());
+    localStorage.clear();
+    console.log(username, token, id);
 
 
-}
+  }
 
 
 
   return (
-    <div>
-         <Link to={"/profile/" + id} className="main-nav-item">
-         <FontAwesomeIcon style={style} icon={faCircleUser} />
+    location.pathname === "/edit/" + id ?
+      <div className='signOut-edit'>
+        <Link to={"/profile/" + id} className="main-nav-a-edit">
+          {username} <img className='profileImg nav-img' src={ProfileEdit} alt='logo de profile' />
+        </Link>
+        <Link className='setting-link main-nav-a-edit'><img className='setting nav-img' src={Settings} alt='logo pour les paramètres' /></Link>
+        <Link onClick={() => { handleClick() }} to="/" className='deconnect-link main-nav-a-edit' ><img className='deconnect nav-img' src={Deconnect} alt='Bouton de déconnection' /></Link>
+      </div>
+      :
+      <div>
+        <Link to={"/profile/" + id} className="main-nav-item">
+          <FontAwesomeIcon style={style} icon={faCircleUser} />
           {username}
         </Link>
-        <Link  onClick={()=> {handleClick()}} to="/" className="main-nav-item">
-        <FontAwesomeIcon style={style} icon={faRightFromBracket} /> 
+        <Link onClick={() => { handleClick() }} to="/" className="main-nav-item">
+          <FontAwesomeIcon style={style} icon={faRightFromBracket} />
           Sign Out
         </Link>
-        
-    </div>
+
+      </div>
+
+
   )
 }
 
--- Front-end/mon-app/src/containers/Header.js
+++ Front-end/mon-app/src/containers/Header.js
@@ -5,6 +5,7 @@ import SignIn from '../components/SignIn';
 import SignOut from '../components/SignOut';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import LogoEdit from "../assets/img/argentBankEdit.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircleUser} from '@fortawesome/free-solid-svg-icons';
 
@@ -13,18 +14,20 @@ import {faCircleUser} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   let id = useSelector((state)=> state.id);
+  let userName = useSelector((state) => state.username);
   const style = {paddingRight:"5px"};
+  const styleEdit= {padding: "28px 50px"}
 const location = useLocation();
 // console.log(location);
 
   return (
     
     <div>
-          <nav className="main-nav">
+          <nav className={location.pathname==="/edit/"+id ? "main-nav-edit" : "main-nav"}>
       <a className="main-nav-logo" href="./index.html">
         <img
           className="main-nav-logo-image"
-          src= {Logo}
+          src={location.pathname === "/edit/" + id  ? LogoEdit : Logo } 
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
