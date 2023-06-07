3c3a6f8125cbfcaba991a259cd70e3442ca660f7
Louisa Ongenyi <o.louisa@ymail.com>
2023-06-07 15:00:02 +0200

    endpoints

--- Front-end/mon-app/public/endpoints-api.js
+++ Front-end/mon-app/public/endpoints-api.js
@@ -3,7 +3,7 @@
 // GET/user/transaction/{transactionId}
 
 
-const GetAllResponse200 = {
+const GetAllTransactions = {
     "status": 200,
     "message": "données récupérées avec succès !",
     "transactions": [
@@ -13,27 +13,15 @@ const GetAllResponse200 = {
             "description": "Golden Sun Bakery",
             "amount": "$8,00",
             "balance": "$298,00",
-            "type": "Electronic",
-            "category": [
-                "Food",
-                "sport",
-                "health"
-            ],
-            "note": "lorem ipsum"
-
-        },
-        {
-            "id": 1,
-            "date": 20200227,
-            "description": "Golden Sun Bakery",
-            "amount": "$8,00",
-            "balance": "$298,00", "type": "Electronic",
-            "category": [
-                "Food",
-                "sport",
-                "health"
-            ],
-            "note": "lorem ipsum"
+            "details": {
+                "type": "Electronic",
+                "category": [
+                    "Food",
+                    "sport",
+                    "health"
+                ],
+                "note": "lorem ipsum"
+            }
         },
         {
             "id": 1,
@@ -41,73 +29,131 @@ const GetAllResponse200 = {
             "description": "Golden Sun Bakery",
             "amount": "$8,00",
             "balance": "$298,00",
-            "type": "Electronic",
-            "category": [
-                "Food",
-                "sport",
-                "health"
-            ],
-            "note": "lorem ipsum"
+            "details": [{
+                "type": "Electronic",
+                "category": [
+                    "Food",
+                    "sport",
+                    "health"
+                ],
+                "note": "lorem ipsum"
+            }]
+        },
+        {
+            "id": 1,
+            "date": 20200227,
+            "description": "Golden Sun Bakery",
+            "amount": "$8,00",
+            "balance": "$298,00",
+            "details": [{
+                "type": "Electronic",
+                "category": [
+                    "Food",
+                    "sport",
+                    "health"
+                ],
+                "note": "lorem ipsum"
+            }]
         }
     ]
 };
-const GetAllResponse2 =[
-{
-    "status": 401,
-    "message": "Token is missing from header"
-},
-{
-    "status" : 400,
-    "message": "erreur dans la requête"
+
+const getOneTransaction = [
+    {
+        "status": 200,
+        "message": "données récupérées avec succès !",
+        "transactions":
+        {
+            "id": 1,
+            "date": 20200227,
+            "description": "Golden Sun Bakery",
+            "amount": "$8,00",
+            "balance": "$298,00",
+            "details": {
+                "type": "Electronic",
+                "category": [
+                    "Food",
+                    "sport",
+                    "health"
+                ],
+                "note": "lorem ipsum"
+            }
+        }
+    },
+
+    {
+        "status": 401,
+        "message": "Token is missing from header"
+    },
+    {
+        "status": 400,
+        "message": "erreur dans la requête"
+    }]
+
+
+// POST/user/transaction/{transactionId}/detail
+const bodyParametersPOST = {
+    "note": "string"
+}
+
+const responseAPI_POST =
+    [{
+        "status": 200,
+        "message": "Ajout réalisé avec succès!"
+    },
+
+    {
+        "status": 401,
+        "message": "Token is missing from header"
+    },
+    {
+        "status": 400,
+        "message": "erreur dans la requête"
+    }
+    ]
+
+// PUT/user/transaction/{transactionId}/detail
+const bodyParametersPUT = {
+    "category": "string",
+    "note": "string"
+}
+const responseApi_PUT =
+    [{
+        "status": 200,
+        "message": "Modification réalisée avec succès!"
+    },
+
+    {
+        "status": 401,
+        "message": "Token is missing from header"
+    },
+    {
+        "status": 400,
+        "message": "erreur dans la requête"
+    }
+    ]
+
+// DELETE/user/transaction/{transactionId}/detail
+const bodyParametersDELETE = [{
+    "category": [
+        "string"
+    ],
+    "note": "string"
+
 }]
-// POST/user/transaction/{transactionId}
 
-const postTransactionInfo = 
-[{
-"status":200,
-"message": "Ajout réalisé avec succès!"
-},
+const responseAPI_DELETE =
+    [{
+        "status": 200,
+        "message": "Suppression réalisée avec succès!"
+    },
 
-{
-    "status" : 401,
-    "message": "Token is missing from header"
-},
-{
-    "status" : 400,
-    "message": "erreur dans la requête"
-}
-]
-
-// PUT/user/transaction/{transactionId}
-const modifTransactionInfo = 
-[{
-"status":200,
-"message": "Modification réalisée avec succès!"
-},
-
-{
-    "status" : 401,
-    "message": "Token is missing from header"
-},
-{
-    "status" : 400,
-    "message": "erreur dans la requête"
-}
-]
-
-// DELETE/user/transaction/{transactionId}
-const deleteTransactionInfo = 
-[{
-"status":200,
-"message": "Suppression réalisée avec succès!"
-},
-
-{
-    "status" : 401,
-    "message": "Token is missing from header"
-},
-{
-    "status" : 400,
-    "message": "erreur dans la requête"
-}
-]
+    {
+        "status": 401,
+        "message": "Token is missing from header"
+    },
+    {
+        "status": 400,
+        "message": "erreur dans la requête"
+    }
+    ]
