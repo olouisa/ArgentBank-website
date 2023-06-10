a4f8853d9befbdd8269043f15e90fc0a1fd321dd
Louisa Ongenyi <o.louisa@ymail.com>
2023-06-10 16:28:11 +0200

    Correction 2

--- Front-end/mon-app/src/containers/Features.js
+++ Front-end/mon-app/src/containers/Features.js
@@ -6,27 +6,22 @@ import Data from "../Lists/features.json";
 import FeaturesItem from '../components/FeaturesItem';
 
 function Features() {
-    console.log(Data);
-    console.log(Data.features)
-    const Tab = Data.features;
+  console.log(Data);
+  console.log(Data.features)
+  const Tab = Data.features;
 
 
   return (
     <div>
-         <section className="features">
+      <section className="features">
         <h2 className="sr-only">Features</h2>
 
         {Data.features.map((item) => {
-        return (
-            <FeaturesItem  key={item.id} title={item.title} text={item.text} picture={require(`../assets/img/${item.picture}`) }/>
+          return (
+            <FeaturesItem key={item.id} title={item.title} text={item.text} picture={require(`../assets/img/${item.picture}`)} />
 
-        )
-       }) }
-
-        {/* <FeaturesItem title={Tab[0].title} text={Tab[0].text} picture={Chat} />
-        <FeaturesItem title={Tab[1].title} text={Tab[1].text} picture={Money} />
-        <FeaturesItem title={Tab[2].title} text={Tab[2].text} picture={Security} /> */}
-
+          )
+        })}
 
       </section>
     </div>
